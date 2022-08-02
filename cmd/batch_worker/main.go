894d5015d07323b6990bfce94a2abf933631b287
KL-Lru <kilattoeruru@outlook.jp>
2022-08-03 01:06:22 +0900

    add timeout settings

--- cmd/batch_worker/main.go
+++ cmd/batch_worker/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/KL-Lru/sample-web-service/pkg/repos"
 )
@@ -13,5 +14,25 @@ func main() {
 		panic("Can't connect SQL")
 	}
 
-	fmt.Println(repo.UserList())
+	timeout := time.After(10 * time.Second)
+	ticker := time.Tick(500 * time.Millisecond)
+	for {
+		select {
+		case <-timeout:
+			fmt.Println("Can't connect SQL")
+			return
+		case <-ticker:
+			err := repo.Ping()
+			if err == nil {
+				users, err := repo.UserList()
+				if err == nil {
+					fmt.Println(users)
+					return
+				} else {
+					fmt.Println("Can't get user info")
+					return
+				}
+			}
+		}
+	}
 }
